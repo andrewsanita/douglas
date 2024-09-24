b29f6230cf3505bfd7b0961088377d731b15966a
andrewsanita <161316038+andrewsanita@users.noreply.github.com>
2024-09-24 11:40:35 -0300

    s

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function soma(num1,num2){
 }
 console.log(soma(2,3));
 
-function multiplicação(num1,num2){
-    return num1*num2;
+function multiplicação(num3,num4){
+    return num3*num4;
 }
 
-console.log(multiplicação(num1,num2));
+console.log(multiplicação(6,6));
