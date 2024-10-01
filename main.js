d8952496ddd1634560de3606e679733891cb577e
andrewsanita <161316038+andrewsanita@users.noreply.github.com>
2024-10-01 11:55:46 -0300

    d

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log(soma(2,3));
+//function soma(num1,num2){//
+//    return num1+num2;
+//}
+//console.log(soma(2,3));//
 
-function multiplicação(num3,num4){
-    return num3*num4;
-}
+//function multiplicação(num3,num4){
+//    return num3*num4;
+//}
 
-console.log(multiplicação(6,6));
+//console.log(multiplicação(6,6));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
+//function areaQuadrado(lado){
+//    return lado**2;
+//}
 
-console.log(areaQuadrado(6))
+//console.log(areaQuadrado(6))
+//
+//let areatrapezio= function(dro,gras,baratas){
+//    return (dro+gras)*baratas/2
+//}
 
-let areatrapezio= function(dro,gras,baratas){
-    return (dro+gras)*baratas/2
-}
+//console.log(areatrapezio(69,24,666))
 
-console.log(areatrapezio(69,24,666))
+//function valores(v1,v2,v3){
+//return v1+v2+v3/3
+//}
 
-function valores(v1,v2,v3){
-return v1+v2+v3/3
-}
+//parseInt(prompt('coloca os 3 numeros ae'))
 
-parseInt(prompt('coloca os 3 numeros ae'))
+//console.log //
 
-console.log 
+let estados = ['santa catarina','rio grande so sul','paraná','são paulo','rio de janeiro','minas gerais','espirito santo','bahia','mato grosso do sul','goiás','mato grosso','tocantins','rondonia','pará','acre','amazonia','roraima','amapa','maranhao','piaui','sergipe','alagoas','pernambuco','paraiba','rio grande do norte'];
+
+let i=0;
+while(i<10)
+    console.log(i);
+console.log(estados[i]);
+i=i+1;
